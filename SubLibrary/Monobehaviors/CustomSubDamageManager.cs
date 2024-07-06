9b3f87e3c89debf30f5eefbe361e2eab54018d76
Indigocoder1 <caleb@liston.us>
2024-07-06 13:17:01 -0700

    Changed how some assigners work

---
+++ SubLibrary/Monobehaviors/CustomSubDamageManager.cs
@@ -0,0 +1,12 @@
+﻿using SubLibrary.CyclopsReferencers;
+using UnityEngine;
+
+namespace SubLibrary.Monobehaviors;
+
+internal class CustomSubDamageManager : CyclopsExternalDamageManager, ICyclopsReferencer
+{
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        fxPrefabs = cyclops.GetComponentInChildren<CyclopsExternalDamageManager>(true).fxPrefabs;
+    }
+}
---
+++ SubLibrary/Monobehaviors/CustomSubVFXConstructing.cs
@@ -0,0 +1,17 @@
+﻿using SubLibrary.CyclopsReferencers;
+using UnityEngine;
+
+namespace SubLibrary.Monobehaviors;
+
+public class CustomSubVFXConstructing : VFXConstructing, ICyclopsReferencer
+{
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        var cyclopsConstructing = cyclops.GetComponent<VFXConstructing>();
+        ghostMaterial = cyclopsConstructing.ghostMaterial;
+        alphaTexture = cyclopsConstructing.alphaTexture;
+        alphaDetailTexture = cyclopsConstructing.alphaDetailTexture;
+        transparentShaders = cyclopsConstructing.transparentShaders;
+        surfaceSplashFX = cyclopsConstructing.surfaceSplashFX;
+    }
+}
