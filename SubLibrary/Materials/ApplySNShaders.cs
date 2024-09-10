8194c1f358dedab695ff2122a42f7ebb76f6914b
Indigocoder1 <caleb@liston.us>
2024-09-10 16:20:40 -0700

    Added apply shaders script

---
+++ SubLibrary/Materials/ApplySNShaders.cs
@@ -0,0 +1,19 @@
+﻿using Nautilus.Utility;
+using UnityEngine;
+
+namespace SubLibrary.Materials;
+
+internal class ApplySNShaders : MonoBehaviour
+{
+    [SerializeField] private GameObject applyTo;
+
+    private void OnValidate()
+    {
+        if (applyTo == null) applyTo = gameObject;
+    }
+
+    private void Start()
+    {
+        MaterialUtils.ApplySNShaders(applyTo);
+    }
+}
