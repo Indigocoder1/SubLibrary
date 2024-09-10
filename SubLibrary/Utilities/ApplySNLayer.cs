42ced6de5ef486fbeab5645aff0d05bb191439ee
Indigocoder1 <caleb@liston.us>
2024-09-09 20:25:42 -0700

    Added layer applier

---
+++ SubLibrary/Utilities/ApplySNLayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace SubLibrary.Utilities;
+
+internal class ApplySNLayer : MonoBehaviour
+{
+    [SerializeField] private LayerName layerName;
+
+    private void Start()
+    {
+        FieldInfo field = typeof(LayerID).GetField(layerName.ToString(), BindingFlags.Static | BindingFlags.Public);
+        gameObject.layer = (int)field.GetValue(null);
+    }
+
+    public enum LayerName
+    {
+        Default,
+        Usable,
+        NotUsable,
+        Player,
+        TerrainCollider,
+        UI,
+        Trigger,
+        BaseClipProxy,
+        OnlyVehicle,
+        Vehicle,
+        SubRigidbodyExclude,
+        DefaultCollisionMask
+    }
+}
