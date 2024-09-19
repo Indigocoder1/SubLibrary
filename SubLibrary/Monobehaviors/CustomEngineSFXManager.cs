338cbf7208b6182aa29d8b31ec9ab45e1a0b1494
Indigocoder1 <caleb@liston.us>
2024-09-18 17:04:22 -0700

    Added channel stuff

--- SubLibrary/Monobehaviors/CustomEngineSFXManager.cs
+++ SubLibrary/Monobehaviors/CustomEngineSFXManager.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using FMOD;
+using Nautilus.Handlers;
+using UnityEngine;
 
 namespace SubLibrary.Monobehaviors;
 
 internal class CustomEngineSFXManager : EngineRpmSFXManager
 {
+    [Tooltip("What volume to be at given a normalized speed value")]
+    [SerializeField] private AnimationCurve volumeOverSpeed;
+
+    private Channel loopingChannel;
+    private bool foundChannel;
+
+    private void Start()
+    {
+        foundChannel = CustomSoundHandler.TryGetCustomSoundChannel(engineRpmSFX.GetInstanceID(), out loopingChannel);
+
+        if (foundChannel)
+        {
+            loopingChannel.setPitch(0);
+        }
+    }
+
     public void Update()
     {
         HandleRevUp();
@@ -56,5 +74,21 @@ internal class CustomEngineSFXManager : EngineRpmSFXManager
         {
             engineRpmSFX.Stop();
         }
+
+        if (!foundChannel) return;
+
+        float volume = volumeOverSpeed.Evaluate(rpmSpeed / topClampSpeed);
+        loopingChannel.setVolume(volume);
+
+        if (Mathf.Approximately(rpmSpeed, 0f))
+        {
+            loopingChannel.getPitch(out var currentPitch);
+            loopingChannel.setPitch(Mathf.MoveTowards(currentPitch, 0, Time.deltaTime * rampDownSpeed));
+        }
+
+        float targetPitch = rpmSpeed / (rpmSpeed * 0.6f) * 2;
+
+        targetPitch = Mathf.Clamp(targetPitch, 0.52f, 1.5f);
+        loopingChannel.setPitch(targetPitch);
     }
 }
