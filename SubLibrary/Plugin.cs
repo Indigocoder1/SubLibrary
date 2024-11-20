951644a5609288c077a1e76ca72eb9ac79fe9da6
Indigocoder1 <caleb@liston.us>
2024-11-19 18:16:56 -0800

    Version increment & package update

--- SubLibrary/Plugin.cs
+++ SubLibrary/Plugin.cs
@@ -14,7 +14,7 @@ public class Plugin : BaseUnityPlugin
 {
     private const string GUID = "com.indigocoder.sublibrary";
     private const string pluginName = "Sub Library";
-    private const string versionString = "1.4.0";
+    private const string versionString = "1.4.1";
 
     public new static ManualLogSource Logger { get; private set; }
 
