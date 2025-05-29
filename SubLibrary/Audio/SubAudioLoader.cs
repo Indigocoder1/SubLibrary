8177b1da2c752c5bbd6bf1b5ee926864646877c0
Indigocoder1 <caleb@liston.us>
2025-05-28 18:44:21 -0700

    Add additional audio loading functions

--- SubLibrary/Audio/SubAudioLoader.cs
+++ SubLibrary/Audio/SubAudioLoader.cs
@@ -16,23 +16,46 @@ public class SubAudioLoader : MonoBehaviour
     /// <param name="bundle">The Asset Bundle to load audio from</param>
     public static void LoadAllAudio(AssetBundle bundle)
     {
-        foreach (var asset in bundle.LoadAllAssets<CustomFMODAsset>())
+        var assets = bundle.LoadAllAssets<CustomFMODAsset>();
+        foreach (var asset in assets)
         {
-            if (asset.doNotAutoRegister) continue;
-
-            string id = asset.id == string.Empty ? asset.path : asset.id;
-            var sound = AudioUtils.CreateSound(asset.audioClip, asset.mode);
-            if (asset.minDistance3D > 0 || asset.maxDistance3D > 0)
-            {
-                sound.set3DMinMaxDistance(asset.minDistance3D, asset.maxDistance3D);
-            }
-
-            if (asset.fadeOutTime > 0)
-            {
-                sound.AddFadeOut(asset.fadeOutTime);
-            }
-
-            CustomSoundHandler.RegisterCustomSound(asset.path, sound, asset.GetBus());
+            RegisterAssetAudio(asset);
         }
+    }
+
+    /// <summary>
+    /// Checks all objects in the provided array, to find <see cref="Audio.CustomFMODAsset"/>s. Once found, loads all assets 
+    /// </summary>
+    /// <param name="allBundleAssets">The assets to search through</param>
+    public static void LoadAllAudio(object[] allBundleAssets)
+    {
+        foreach (var obj in allBundleAssets)
+        {
+            if (obj is not CustomFMODAsset asset) continue;
+
+            RegisterAssetAudio(asset);
+        }
+    }
+
+    /// <summary>
+    /// Registers the provided CustomFMODAsset with Nautilus, given that <see cref="Audio.CustomFMODAsset.doNotAutoRegister"/> is not set to true
+    /// </summary>
+    /// <param name="asset">The asset to register</param>
+    public static void RegisterAssetAudio(CustomFMODAsset asset)
+    {
+        if (asset.doNotAutoRegister) return;
+            
+        var sound = AudioUtils.CreateSound(asset.audioClip, asset.mode);
+        if (asset.minDistance3D > 0 || asset.maxDistance3D > 0)
+        {
+            sound.set3DMinMaxDistance(asset.minDistance3D, asset.maxDistance3D);
+        }
+
+        if (asset.fadeOutTime > 0)
+        {
+            sound.AddFadeOut(asset.fadeOutTime);
+        }
+
+        CustomSoundHandler.RegisterCustomSound(asset.path, sound, asset.GetBus());
     }
 }
